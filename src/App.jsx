844d46b8afb02eb1d917a33f3f9e8de1d43f8d99
Daniel Alejandro Perea <aledlfuente11@gmail.com>
2024-07-23 19:45:53 -0300

    avances del login

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import FormInicio from './folder/formInicio';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+     
+      <FormInicio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/folder/formInicio.jsx
@@ -0,0 +1,27 @@
+import "./formInicio.css"
+import {FaUser, FaLock} from "react-icons/fa"
+export function FormInicio(){
+    return(
+       
+        <body> 
+             <img src="NucleoColor.png" alt=""/>
+        <section>
+            <form className="formulario">
+            <h1>Iniciar sesión</h1>
+            <label>
+                <FaUser className="icon"/>
+                <input type="text" placeholder="Nombre de usuario"/>  
+            </label>
+            <label>
+                <FaLock className="icon"/>
+                <input type="password" placeholder="Contraseña"/>
+            </label>
+                <a href="#">Recuperar contraseña</a>
+                <button>Ingresar</button>
+            </form> 
+
+        </section>
+        </body>
+    )
+}
+export default FormInicio;
