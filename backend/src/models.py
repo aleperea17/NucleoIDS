b1da12541e4e15c33aa61dbc76366050107ee827
Arturo Iván Artaza <aartazaesc@gmail.com>
2024-08-25 21:33:22 -0300

    fix(backend): fixed some stuff

--- backend/src/models.py
+++ backend/src/models.py
@@ -3,8 +3,6 @@ from pony.orm import *
 from enum import Enum
 from .db import db
 
-<<<<<<< Updated upstream
-=======
 
 class Roles(str, Enum):
     ADMIN = "ADMIN"
@@ -12,20 +10,14 @@ class Roles(str, Enum):
     TEACHER = "TEACHER"
 
 
->>>>>>> Stashed changes
 class User(db.Entity):
     id = PrimaryKey(uuid.UUID, auto=True)
     username = Required(str)
     email = Required(str)
     password = Required(str)
     firstName = Required(str, column="firstName")
-<<<<<<< Updated upstream
-    lastName = Required(str,column="lastName")
-    role = Enum("Roles",["ADMIN", "STUDENT", "TEACHER"])
-    _table_ = "Users"
-
-=======
     lastName = Required(str, column="lastName")
     role = Required(Roles, default=Roles.STUDENT)
     _table_ = "Users"
->>>>>>> Stashed changes
+
+
