47ca26c1872e5101e0a78bca8eeaf6f2050984e1
Arturo Iván Artaza <aartazaesc@gmail.com>
2024-08-28 15:34:14 -0300

    merged branchs manually

---
+++ backend/src/db.py
@@ -0,0 +1,7 @@
+from pony.orm import *
+
+db = Database()
+
+# Binding, establecer la conexión con la base de datos.
+db.bind(provider='postgres', user='mydb_owner', password='M4ZjNOokxQI0',
+        host='ep-frosty-surf-a5o852lb.us-east-2.aws.neon.tech', database="mydb")
