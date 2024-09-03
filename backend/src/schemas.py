fab8c08c2b774c60dc24829e008d2e139023f21c
Arturo Iván Artaza <aartazaesc@gmail.com>
2024-09-02 22:24:56 -0300

    feat(backend): implementado auth para register y login

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -1,24 +1,33 @@
 from pydantic import BaseModel
 
+from src.models import Roles
+
+
 class BaseUser(BaseModel):
     username: str
-    email:str 
-    firstName : str
+    email: str
+    firstName: str
     lastName: str
+    role: Roles
 
     class Config:
-        #antes orm_mode=True
-        from_attributes=True
+        # antes orm_mode=True
+        from_attributes = True
+
 
 class UserCreate(BaseUser):
-    password:str
+    password: str
 
 # Modelo de entrada
+
+
 class LoginRequest(BaseModel):
-    username: str | None= None
+    username: str | None = None
     email: str | None = None
-    password: str 
+    password: str
 
 # Modelo para recibir la imagen en base64 del front.
+
+
 class ImageRequest(BaseModel):
-    image_base64:str
+    image_base64: str
