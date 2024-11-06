a3409072e810e8d90d4efbf096f743b9e53da224
Facundo Maidana <facumaidana2001@gmail.com>
2024-11-06 15:49:42 -0300

    Cambiar titulo y logo de pagina

--- frontend/src/components/table/table.jsx
+++ frontend/src/components/table/table.jsx
@@ -1,41 +1,43 @@
-const Table = ({ title, data, columns }) => {
-	return (
-		<div className="">
-			<div className="hero">
-				<div className="max-w-md">
-					<h1 className="text-2xl font-bold">{title}</h1>
-					<br />
-				</div>
-			</div>
+import fotos from "./img";
 
-			<table className="table">
-				{/* head */}
-				<thead>
-					<tr>
-						{columns.map((column, index) => (
-							<th key={`table-column-${index}`} className="text-xl">
-								{column.header}
-							</th>
-						))}
-					</tr>
-				</thead>
-				<tbody>
-					{data.map((row, rowIndex) => (
-						<tr key={rowIndex}>
-							{columns.map((column, colIndex) => (
-								<td key={colIndex}>
-									{column.render
-										? column.render(row[column.accessor], row)
-										: row[column.accessor]}
-								</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-				<tfoot></tfoot>
-			</table>
-		</div>
-	);
+const Table = ({ title, data, columns }) => {
+  return (
+    <div className="">
+      <div className="hero">
+        <div className="max-w-md">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <br />
+        </div>
+      </div>
+
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th key={`table-column-${index}`} className="text-xl">
+                {column.header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((column, colIndex) => (
+                <td key={colIndex}>
+                  {column.render
+                    ? column.render(row[column.accessor], row)
+                    : row[column.accessor]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
